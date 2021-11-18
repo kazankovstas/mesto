2466a0e62808c5c69cc9e069992b3c5ea7547af9
Stanislav <kazankovstas.jb@gmail.com>
2021-11-18 12:44:35 +0300

    a

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,16 @@ const aboutLink = document.querySelector(".profile__button-edit");
 const popup = document.querySelector(".popup");
 const popupCloseButton = document.querySelector(".popup__close");
 const saveButton = document.querySelector(".form__button-save");
+const profileName = document.querySelector(".profile__name");
+const profileText = document.querySelector(".profile__text");
+const formElement = document.querySelector(".form");
+const nameInput = document.querySelector(".form__name");
+const aboutInput = document.querySelector(".form__about");
 
 function openPopup() {
   popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileText.textContent;
 }
 
 function closePopup() {
@@ -13,21 +20,12 @@ function closePopup() {
 
 aboutLink.addEventListener("click", openPopup);
 popupCloseButton.addEventListener("click", closePopup);
-saveButton.addEventListener("click", closePopup);
-
-const profileName = document.querySelector(".profile__name");
-const profileText = document.querySelector(".profile__text");
-const formElement = document.querySelector(".form");
-const nameInput = document.querySelector(".form__name");
-const aboutInput = document.querySelector(".form__about");
-
-nameInput.value = profileName.textContent;
-aboutInput.value = profileText.textContent;
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   (profileName.textContent = nameInput.value),
-    (profileText.textContent = aboutInput.value);
+    (profileText.textContent = aboutInput.value),
+    closePopup();
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
